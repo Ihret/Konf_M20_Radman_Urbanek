e4dfce9e017329dd20615599361746f3c2d4240e
Mario Radman <mario.radman@edu.fh-joanneum.at>
2021-04-24 13:46:38 +0200

    Changes output of Drinks Main

--- src/main/java/at/fhj/iit/Main.java
+++ src/main/java/at/fhj/iit/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args){
 
         Liquid l = new Liquid("Wein", 0.125, 13);
-        System.out.println(l.getName());
-        System.out.println(l.getVolume());
 
         Drink d = new SimpleDrink("Rotwein",l);
         System.out.println(d);
